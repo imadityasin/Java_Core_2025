3b51ffc35f26f44ea0fb9c486d5cba0ff835ff02
Aditya Singh <adityasingh1803001@gmail.com>
2025-05-27 16:20:01 +0530

    encapsulation 2nd and 1st program done

--- src/oops/encapsulation/Program_01.java
+++ src/oops/encapsulation/Program_01.java
@@ -1,5 +1,70 @@
 //Encapsulation.
+
+//1. Basic Student Info (Beginner)
+//Create a class Student with the following private fields:
+//
+//name, age, rollNumber
+//Add public getter and setter methods for each.
+//Then create a main class to set and display the values.
+
 package oops.encapsulation;
 
+import java.util.Scanner;
+
+class Student{
+        private String name;
+        private int age;
+        private long rollNumber;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRollNumber(long rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getRollNumber() {
+        return rollNumber;
+    }
+}
+
 public class Program_01 {
+    public static void main(String[] args) {
+//        Student class Object for calling methods
+        Student st=new Student();
+
+//        Scanner class object for Taking user input
+        Scanner sc=new Scanner(System.in);
+
+//        Taking details from user and strroing then passing
+        System.out.println("Enter Name : ");
+        String name=sc.nextLine();
+        st.setName(name);
+
+        System.out.println("Enter Age : ");
+        int Age=sc.nextInt();
+        st.setAge(Age);
+
+        System.out.println("Enter Roll Number");
+        long rollNumber= sc.nextLong();
+        st.setRollNumber(rollNumber);
+
+//        Displaying Details
+        System.out.println("Name : "+st.getName());
+        System.out.println("Age : "+st.getAge());
+        System.out.println("Roll Number : "+st.getRollNumber());
+    }
 }
--- src/oops/encapsulation/Program_02.java
+++ src/oops/encapsulation/Program_02.java
@@ -1,6 +1,116 @@
 //Encapsulation.
 
+//2. Bank Account with Balance Check (Intermediate)
+//Create a class BankAccount with:
+//
+//Private fields: accountHolderName, accountNumber, and balance
+//
+//Public methods:
+//
+//deposit(double amount)
+//
+//withdraw(double amount) – only allow if balance is sufficient
+//
+//getBalance()
+//
+//Use encapsulation to protect balance from direct access.
+
 package oops.encapsulation;
 
+import java.util.Scanner;
+
+class BankAccount{
+    private String accountHolderName;
+    private long accountNumber;
+    private double balance;
+
+    public void setAccountHolderName(String accountHolderName){
+        this.accountHolderName=accountHolderName;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void setDeposit(double amount){
+        balance=balance+amount;
+        System.out.println("Deposit Successfully Updated Balance is : "+balance);
+    }
+
+    public void setWithdraw(double amount){
+        if(balance>=amount) {
+            balance=balance-amount;
+            System.out.println("Amount Withdraw Successfully of ruppes : "+amount);
+            System.out.println("Withdraw Successfully Updated Balance is : "+balance);
+        }
+        else{
+            System.out.println("Insufficient Balance");
+        }
+    }
+}
 public class Program_02 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        BankAccount ba=new BankAccount();
+
+//        Taking values form user because we are studying but this data comes from DB.
+        System.out.println("Enter Your Name : ");
+        String name=sc.nextLine();
+        ba.setAccountHolderName(name);
+
+        System.out.println("Enter Your Account Number : ");
+        long accnum= sc.nextLong();
+        ba.setAccountNumber(accnum);
+
+        System.out.println("Enter Your Balance : ");
+        double bal=sc.nextDouble();
+        ba.setBalance(bal);
+
+        System.out.println("Now, we are Redirecting you to ATM Service: ");
+        System.out.println("=========================Loading 100%================================");
+        System.out.println("==========================Displaying.....===============================");
+
+        System.out.println("Press 1: for DEPOSIT");
+        System.out.println("Press 2: for WITHDRAW");
+        System.out.println("Press 3: for BALANCE");
+        int choice=sc.nextInt();
+
+        switch (choice) {
+            case 1: {
+                System.out.println("Current Account Balance is : "+ba.getBalance());
+                System.out.println("Enter Amount to Deposit : ");
+                double amount=sc.nextDouble();
+                ba.setDeposit(amount);
+                break;
+            }
+
+            case 2: {
+                System.out.println("Current Account Balance is : "+ba.getBalance());
+                System.out.println("Enter Amount To Withdraw : ");
+                double amount=sc.nextDouble();
+                ba.setWithdraw(amount);
+                break;
+            }
+
+            case 3: {
+                System.out.println("Your Current Savings Account Balance is : "+ba.getBalance());
+                break;
+            }
+
+            default:{
+                System.out.println("Invalid input...please Try Again...!");
+            }
+        }
+
+
+    }
 }
